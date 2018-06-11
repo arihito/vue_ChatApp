65e1c0656308b87f1953b4ea734a7ef10ae62934
arihito <arihito.m@gmail.com>
2018-06-11 14:17:47 +0900

    base routing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
+<template lang="pug" src="./app.pug" />
 
 <script>
 export default {
@@ -12,12 +7,34 @@ export default {
 </script>
 
 <style>
+:root {
+  --sample-weight: 900;
+  font-size: 10px;
+}
+html,body {
+  margin: 0;
+  padding: 0;
+  font-size: 1.4rem;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #6c7ea0;
+  background-color: #222;
 }
+  .sample {
+    font-weight: var(--sample-weight);
+    font-size: 2rem;
+    overflow: hidden;
+    padding: 10px;
+    text-align: left;
+  }
+    img {
+      width: 30px;
+      float: left;
+      margin-right: 10px;
+    }
+
 </style>
