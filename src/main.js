59216ebae271afc9549f92a6a1a6b59dccc48f13
arihito <arihito.m@gmail.com>
2018-06-11 16:50:53 +0900

    vuex edit main.js

--- src/main.js
+++ src/main.js
@@ -3,15 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import stote from './store'
+import store from './store'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
-  template: '<App/>',
-  store
+  template: '<App/>'
 })
