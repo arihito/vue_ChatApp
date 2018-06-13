a328b6175bd60101a326fbc2c902bae748827a00
arihito <arihito.m@gmail.com>
2018-06-13 18:53:05 +0900

    messege brushup

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -1,25 +1,31 @@
 import { mapGetters, mapActions } from 'vuex'
-import { SET_MESSAGE } from '../../store/mutation-types'
+import { GET_CHANNELS } from '../../store/mutation-types'
 
 export default {
   name: 'chat',
+  mounted(){
+    this.GET_CHANNELS()
+    this.GET_MESSAGES(this.$route.params.cname)
+  },
   computed: {
     ...mapGetters([
-      'messages'
+      'messages',
+      'channels'
     ]),
   },
   methods: {
     ...mapActions([
-      SET_MESSAGE
+      GET_CHANNELS,
+      "GET_MESSAGES",
+      "POST_MESSAGES"
     ]),
     send_message() {
-      this.SET_MESSAGE(this.message)
+      this.POST_MESSAGES({"cname": this.$route.params.cname, "message": this.message})
       this.message = ""
     }
   },
   data() {
     return {
-      channels: ["general", "random"],
       message: ""
     }
   }
